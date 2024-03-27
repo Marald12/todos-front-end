81b69df06a77e1c28eec8403e7084aa7365ec7a5
Marald12 <catiklou@gmail.com>
2024-03-27 22:41:29 +0200

    init fsd structures and add provider react-query

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['localhost']
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:3001/api/:path*'
+            },
+            {
+                source: '/uploads/:path*',
+                destination: 'http://localhost:3001/uploads/:path*'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
